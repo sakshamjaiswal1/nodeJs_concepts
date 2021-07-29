ce30440cd40abc834c07ede2c8cdef48122f9605
Saksham Jaiswal <sakshamjaiswal@six30labs.io>
2021-07-29 18:34:21 +0530

    path module

---
+++ OsModules/index.js
@@ -0,0 +1,9 @@
+const os = require("os");
+
+console.log(os.arch());
+
+const freeMemory = os.freemem();
+console.log(`${freeMemory / 1024 / 1024 / 1024}`);
+
+const totalMemory = os.totalmem();
+console.log(`${totalMemory / 1024 / 1024 / 1024}`);
