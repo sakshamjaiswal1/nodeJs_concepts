0457eac743c6bcaa53af941204f70ddf0af79b54
Saksham Jaiswal <sakshamjaiswal@six30labs.io>
2021-07-30 00:12:47 +0530

    creating own modules

---
+++ ImportExport/index.js
@@ -0,0 +1,6 @@
+const { add, sub } = require("./oper");
+
+console.log(add(5, 5));
+console.log(sub(5, 5));
+
+// console.log(oper);
---
+++ ImportExport/oper.js
@@ -0,0 +1,14 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+const name = "Saksham";
+
+// module.exports = add;
+// module.exports.add = add;
+// module.exports.sub = sub;
+
+module.exports = { add, sub };
