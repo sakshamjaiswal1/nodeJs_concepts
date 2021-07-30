b9381fa73f3abe16378c74b5804fb0f83e903c42
Saksham Jaiswal <sakshamjaiswal@six30labs.io>
2021-07-31 00:54:38 +0530

    Installing Nodemon

--- npmMod/index.js
+++ npmMod/index.js
@@ -2,5 +2,6 @@ const chalk = require("chalk");
 const validator = require("validator");
 
 console.log(chalk.red.underline.inverse("false"));
-const res = validator.isEmail("thapa@.com");
+const res = validator.isEmail("thapa@gmail.com");
 console.log(res ? chalk.green.inverse(res) : chalk.inverse(res));
+// Saksham jaiswal
